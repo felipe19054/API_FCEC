440243e39fec440137799819d34fa48dfd918da6
manuel <manuelfelipe1713@gmail.com>
2024-09-15 15:14:35 -0500

    Modulo Tres: Gestor de pedidos

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const conectarDB =require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const productosRoutes = require('./routes/productosRoutes');
+const pedidosRoutes = require('./routes/pedidosRoutes');  // Importar rutas de pedidos
 
 // Conectar base de datos
 conectarDB();
@@ -12,12 +13,14 @@ const app = express();
 // Middleware para analizar el cuerpo de las solicitudes
 app.use(express.json());
 
-
 // Usar rutas de autenticación
 app.use('/api/auth', authRoutes);
 
 // Rutas de productos
 app.use('/api', productosRoutes); // Usar rutas de productos
+
+//Rutas de pedidos
+app.use('/api', pedidosRoutes); // Usar rutas de pedidos
 
 // Configurar el puerto en el que escuchará el servidor 
 const PORT = 5000;
